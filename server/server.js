8ec90db9f0fac60bd35bc0389d87fa8d82bdbeb2
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-05-31 11:15:21 +0530

    addon

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const path =require('path');
+const http=require('http');
 const pathP=path.join(__dirname,'../public');
 const express=require('express');
+const socketIO=require('socket.io');
 
-var port=process.env.PORT || 3000;
+var port=process.env.PORT || 3000
 
 var app =express();
-
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(pathP));
 
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+    socket.on('disconnect',()=>{
+        console.log('user was disconnected');
+    })
+});
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
     console.log(`server is running on ${port}`)
 })
